30af959ee2cbf628f4c70da1d6d55be35c18813d
yananob <yananob@gmail.com>
2024-11-30 18:11:04 +0900

    separated

--- deploy/deploy_php_http.sh
+++ deploy/deploy_php_http.sh
@@ -2,12 +2,12 @@
 set -eu
 
 DEPLOY_DIR=_deploy
-export SCRIPT_NAME=$1
+export SCRIPT_NAME=line-ai-bot
 # remove "/" on the right side
 SCRIPT_NAME=`php -r '$result=getenv("SCRIPT_NAME"); echo substr($result, -1) === "/" ? rtrim($result, "/") : $result;'`
 
 echo "Checking ${SCRIPT_NAME}"
-pushd ${SCRIPT_NAME}
+# pushd ${SCRIPT_NAME}
 
 # Check existance of .gcloudignore
 if ! test -f ".gcloudignore"; then
@@ -15,30 +15,28 @@ if ! test -f ".gcloudignore"; then
     exit 1
 fi
 
-# Check existance of specific deploy.sh
-if test -f "deploy.sh"; then
-    echo "Specific deploy.sh for this app exists. Please run it instead of this shell."
-    exit 1
-fi
+# # Check existance of specific deploy.sh
+# if test -f "deploy.sh"; then
+#     echo "Specific deploy.sh for this app exists. Please run it instead of this shell."
+#     exit 1
+# fi
 
 # check existance of config.sample.json & config.json
-if test -f "config.json.sample"; then
-    if test ! -f "config.json"; then
+if test -f "configs/config.json.sample"; then
+    if test ! -f "configs/config.json"; then
         echo "Config.json.sample exists. Please make config.json for this app."
         exit 1
     fi
 fi
-popd
+# popd
 
 echo "Starting to deploy ${SCRIPT_NAME}"
 
+rm -rf ./${DEPLOY_DIR}
 mkdir -p ${DEPLOY_DIR}
 pushd ${DEPLOY_DIR}
 
-rm -rf ./${SCRIPT_NAME}
-rsync -vaL --exclude-from=../_misc/deploy/rsync_exclude.conf ../${SCRIPT_NAME} ./
-
-pushd ${SCRIPT_NAME}
+rsync -vaL --exclude-from=../cf-common/deploy/rsync_exclude.conf ../ ./
 
 echo "-------- deploying http --------"
 gcloud functions deploy ${SCRIPT_NAME} \
@@ -52,5 +50,4 @@ gcloud functions deploy ${SCRIPT_NAME} \
     --max-instances 1
 
 popd
-rm -rf ./${SCRIPT_NAME}
-popd
+rm -rf ./${DEPLOY_DIR}
