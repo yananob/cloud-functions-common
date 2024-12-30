1d875c94af063522792fea2e787b61b00d58573f
yananob <yananob@gmail.com>
2024-12-30 09:24:23 +0900

    rename

--- deploy/RENAME_deploy.sh
+++ deploy/RENAME_deploy.sh
@@ -3,4 +3,4 @@ set -eu
 
 # choose deploy type
 # bash ./_cf-common/deploy/deploy_php_http.sh . {CLOUD_FUNCTION_NAME}
-# bash ./_cf-common/deploy/deploy_php_topic.sh . {CLOUD_FUNCTION_NAME}
+# bash ./_cf-common/deploy/deploy_php_event.sh . {CLOUD_FUNCTION_NAME}
