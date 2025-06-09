35fe595c853ac2bbcc6d8e673c130f4a5414752d
yananob <yananob@gmail.com>
2025-06-09 20:09:34 +0900

    test scripts

---
+++ test/unset_secrets.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eu
+
+# if [ "$#" -ne 1 ]; then
+#   echo ""
+#   echo "  Invalid arguments."
+#   echo "  Usage: $0 \"SECRET_KEY1,SECRET_KEY2\""
+#   echo ""
+#   exit 1
+# fi
+
+# Remove credentials
+# IFS=',' read -ra SECRETS <<< "$1"
+for secret in "${SECRETS[@]}"; do
+    unset "$secret"
+done
