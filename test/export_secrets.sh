ada184e19b0cbb931847bf559172ef84cdcbed12
yananob <yananob@gmail.com>
2026-03-28 12:57:33 +0900

    crlf

--- test/export_secrets.sh
+++ test/export_secrets.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 set -eu
 
-# if [ "$#" -ne 1 ]; then
-#   echo ""
-#   echo "  Invalid arguments."
-#   echo "  Usage: $0 \"SECRET_KEY1,SECRET_KEY2\""
-#   echo ""
-#   exit 1
-# fi
-
 echo "Exporting secrets..."
 
 # IFS=',' read -ra SECRETS <<< "$1"
